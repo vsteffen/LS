d9d3630d820dacdc0623900f7b1f211830d07a94
Vivien STEFFEN <vsteffen@e1r6p4.42.fr>
2016-03-15 19:32:28 +0100

    Beginning, time to pull the fingers out the ass !

---
+++ include/ft_ls.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ls.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vsteffen <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/03/15 19:28:04 by vsteffen          #+#    #+#             */
+/*   Updated: 2016/03/15 19:29:00 by vsteffen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_LS_H
+# define FT_LS_H
+
+# include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
+# include <fcntl.h>
+# include "libft.h"
+
+typedef struct		data_s
+{
+	int		b;
+}			data_t;
+
+#endif
