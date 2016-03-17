93d965ec3d7f2e067584e0e3d8ee67fd2610caa7
Vivien STEFFEN <vsteffen@e2r2p21.42.fr>
2016-03-17 16:48:14 +0100

    Learning

--- include/ft_ls.h
+++ include/ft_ls.h
@@ -6,7 +6,7 @@
 /*   By: vsteffen <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/15 19:28:04 by vsteffen          #+#    #+#             */
-/*   Updated: 2016/03/15 19:29:00 by vsteffen         ###   ########.fr       */
+/*   Updated: 2016/03/16 15:49:49 by vsteffen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,15 @@
 # include <stdio.h>
 # include <string.h>
 # include <fcntl.h>
+
+#include <dirent.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#include <time.h>
+
+#include <sys/xattr.h>
+
 # include "libft.h"
 
 typedef struct		data_s
--- srcs/maintest.c
+++ srcs/maintest.c
@@ -6,13 +6,90 @@
 /*   By: vsteffen <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/23 16:18:34 by vsteffen          #+#    #+#             */
-/*   Updated: 2016/03/15 19:27:19 by vsteffen         ###   ########.fr       */
+/*   Updated: 2016/03/16 16:48:10 by vsteffen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
+/*
+   void listdir(const char *name, int level)
+   {
+   DIR *dir;
+   struct dirent *entry;
+
+   if (!(dir = opendir(name)))
+   return;
+   if (!(entry = readdir(dir)))
+   return;
+
+   do {
+   if (entry->d_type == DT_DIR) {
+   char path[1024];
+   int len = snprintf(path, sizeof(path)-1, "%s/%s", name, entry->d_name);
+   path[len] = 0;
+   if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
+   continue;
+   printf("%*s[%s]\n", level*2, "", entry->d_name);
+   listdir(path, level + 1);
+   }
+   else
+   printf("%*s- %s\n", level*2, "", entry->d_name);
+   } while (entry = readdir(dir));
+   closedir(dir);
+   }*/
+
+void	stat_function()
+{
+	struct	stat sb;
+	int		ret;
+
+	ret = stat("/nfs/2015/v/vsteffen/LS/Makefile", &sb);
+
+	printf("Numéro d'inœud :                   %ld\n", (long) sb.st_ino);
+
+	printf("Mode :                             %lo (octal)\n",
+			(unsigned long) sb.st_mode);
+
+	printf("Nombre de liens :                  %ld\n", (long) sb.st_nlink);
+	printf("Propriétaires :                    UID=%ld   GID=%ld\n",
+			(long) sb.st_uid, (long) sb.st_gid);
+
+	printf("Taille de bloc d’E/S :             %ld octets\n",
+			(long) sb.st_blksize);
+	printf("Taille du fichier :                %lld octets\n",
+			(long long) sb.st_size);
+	printf("Blocs alloués :                    %lld\n",
+			(long long) sb.st_blocks);
+
+	printf("Dernier changement d’état :        %ld\n", sb.st_ctime);
+	printf("Dernier accès au fichier :         %s", ctime(&sb.st_atime));
+	printf("Dernière modification du fichier:  %s", ctime(&sb.st_mtime));
+
+
+}
+
+void	time_function(void)
+{
+	time_t temps;
+
+	time(&temps);
+	printf("%s", ctime(&temps));
+}
+
+void	learning_function()
+{
+	printf("\n\t-----------------\n");
+	time_function();
+	printf("\n\t-----------------\n");
+	stat_function();
+	printf("\n\t-----------------\n");
+	//	listdir(".", 0);
+}
+
+
 int		main(int ac, char **av)
 {
+	learning_function();
 	return (EXIT_SUCCESS);
 }
